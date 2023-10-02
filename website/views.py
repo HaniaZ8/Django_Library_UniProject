6bf3a072206d58c4f5a8959555cbe04308d8928e
HaniaZ8 <hannazajko2004@gmail.com>
2023-10-02 13:13:50 +0200

    Added navigation bar, log in/out mechanism and messages. Next step: register mechanism

--- website/views.py
+++ website/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    #Checking to see if logging in
+    if request.method == "POST":
+        email_adress = request.POST['email_adress']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=email_adress, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"Logowanie udalo sie!")
+            return redirect('home')
+        else:
+            messages.success(request, "Logowanie nie udalo sie. Sprobuj ponownie..")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Zostales wylogowany...")
+    return redirect('home')
