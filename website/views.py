ebb6f028ef2586b8220b40119d4902f9a4d59aff
HaniaZ8 <hannazajko2004@gmail.com>
2023-10-14 14:04:09 +0200

    Records can be seen after login. Next: make it pretty

--- website/views.py
+++ website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
     #Checking to see if logging in
     if request.method == "POST":
         username = request.POST['username']
@@ -18,7 +21,7 @@ def home(request):
             messages.success(request, "Logowanie nie udalo sie. Sprobuj ponownie..")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
