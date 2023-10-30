b0d5c49e71ea6dc6c0ea64a0276f5c707e493635
HaniaZ8 <hannazajko2004@gmail.com>
2023-10-30 15:24:51 +0100

    Individual records seen, now able to delete them too. Next: adding records

--- website/views.py
+++ website/views.py
@@ -46,3 +46,22 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Look up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You Must be Logged in to view Page")
+        return redirect('home')
+    
+def delete_record(request,pk):
+    if request.user.is_authenticated:    
+        delete_it = Record.objects.get(id = pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully")
+        return redirect('home')
+    else:
+         messages.success(request, "You Must be Logged in to do That")
+         return redirect('home')
