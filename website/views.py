d1ebd6863b6e21001aba86b11ab7d8caf69d6c08
HaniaZ8 <hannazajko2004@gmail.com>
2023-10-31 12:43:35 +0100

    Finished the basic crm. Next: adding personal requirements

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -65,3 +65,17 @@ def delete_record(request,pk):
     else:
          messages.success(request, "You Must be Logged in to do That")
          return redirect('home')
+    
+def update_record(request,pk):
+     if request.user.is_authenticated:    
+        current_record = Record.objects.get(id = pk)
+        form = AddRecordForm(request.POST or None, instance = current_record)
+        if form.is_valid():
+             form.save()
+             messages.success(request, "Record has been Updated!")
+             return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+     else:
+          messages.success(request, "You Must be Logged in to do That")
+          return redirect('home')
+        
